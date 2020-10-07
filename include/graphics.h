3512f90feb4c3554a81f24decbd999b3e48123d4
profande <fidlyjones@gmail.com>
2020-10-06 22:33:33 -0400

    Added documentation

--- include/graphics.h
+++ include/graphics.h
@@ -5,5 +5,8 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+// Initializes Vulkan and returns a GLFWwindow *
 GLFWwindow *InitGraphics(uint32 w, uint32 h, const char *name);
+
+// Deinitializes Vulkan and terminates GLFW
 void GraphicsCleanup();
